935a659199a3d53cd3b8aa8b11950beb0671f422
Spenser Black <spenserblack01@gmail.com>
2020-12-16 15:08:48 -0500

    Populate sentence markov

--- markov.go
+++ markov.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-var markov = make(map[string][]string)
+type Markov struct {
+	Chain map[string][]string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	markov := NewSentence("hello goodbye hello go hello world", 1)
+	fmt.Println(markov)
+}
+
+func NewSentence(words string, prefixLen int) Markov {
+	chain := make(map[string][]string)
+	markov := Markov{chain}
+
+	splitWords := strings.Split(words, " ")
+
+	for i, suffix := range splitWords[prefixLen:] {
+		prefix := strings.Join(splitWords[i:i+prefixLen], " ")
+
+		if suffixes, ok := chain[prefix]; ok {
+			chain[prefix] = append(suffixes, suffix)
+		} else {
+			chain[prefix] = make([]string, 1)
+			chain[prefix][0] = suffix
+		}
+	}
+
+	return markov
 }
