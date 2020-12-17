657f365fefc659d57e753d93722af68615e12ea9
Spenser Black <spenserblack01@gmail.com>
2020-12-17 13:43:42 -0500

    Take optional len in test script

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-go run markov.go "Hello World Hello Go Goodbye World Goodbye Go Goodbye Markov Hello Markov Go Markov Go Go Go"
+LEN=${1:-"1"}
+go run markov.go -n $LEN "Hello World Hello Go Goodbye World Goodbye Go Goodbye Markov Hello Markov Go Markov Go Go Go"
